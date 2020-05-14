64528e15aefdc59ca9e62bd0a3fd83451d0e4da1
IrinaM-a <ruby19@bk.ru>
2020-05-15 00:55:27 +0500

    888

--- barbershop.rb
+++ barbershop.rb
@@ -12,10 +12,26 @@ post '/' do
       @title = "Thank you"
       @message ="Dear #{@user_name}, we'll be waiting for you at  #{@date_time}"
       
-      f = File.open 'users.txt', 'a'
-      f.write "User: #{@user_name}, Phone: #{@phone}, Date and time: #{@date_time}"
-      f.close
+      @f = File.open 'users.txt', 'a'
+      @f.write "User: #{@user_name}, Phone: #{@phone}, Date and time: #{@date_time}"
+      @f.close
       
       erb :message
+end
+
+get '/admin' do
+      erb :admin
+end
+
+post '/admin' do
+      @email    = params[:email]
+      @password = params[:password]
+
+      if @email == 'metlyakova-i@mail.ru' && @password == 'angel123'
+            @a = File.readlines 'users.txt'
+            erb :welcome
+      else
+            erb :admin
+      end
 end
 
